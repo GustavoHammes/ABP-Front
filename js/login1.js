31a487907d095c8a85cb4e7b8175533815fbf2a9
Gustavo Hammes <151464303+GustavoHammes@users.noreply.github.com>
2024-10-21 12:16:49 -0300

    Update login1.js

--- js/login1.js
+++ js/login1.js
@@ -1,19 +1,23 @@
-const wrapper = document.querySelector(".wrapper")
-const loginLink = document.querySelector(".login-link")
-const registerLink = document.querySelector(".register-link")
-const btnPopup = document.querySelector(".btnlogin-popup")
-const iconClose = document.querySelector(".icon-close")
-registerLink.addEventListener("click", () => {
-  wrapper.classList.add("active")
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelector(".wrapper")
+  const loginLink = document.querySelector(".login-link")
+  const registerLink = document.querySelector(".register-link")
+  const btnPopup = document.querySelector(".btnlogin-popup")
+  const iconClose = document.querySelector(".icon-close")
 
-loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active")
-})
+  registerLink.addEventListener("click", () => {
+    wrapper.classList.add("active")
+  })
 
-btnPopup.addEventListener("click", () => {
-  wrapper.classList.add("active-popup")
-})
-iconClose.addEventListener("click", () => {
-  wrapper.classList.remove("active-popup")
+  loginLink.addEventListener("click", () => {
+    wrapper.classList.remove("active")
+  })
+
+  btnPopup.addEventListener("click", () => {
+    wrapper.classList.add("active-popup")
+  })
+
+  iconClose.addEventListener("click", () => {
+    wrapper.classList.remove("active-popup")
+  })
 })
